1c175c59c2e848eb7de51d54970c1a306bb3df82
phamngocthi <pnthi.it.cp@gmail.com>
2024-04-11 00:50:36 +0700

    update handle dataset

--- pre_dataset.py
+++ pre_dataset.py
@@ -8,6 +8,7 @@ from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
 from bs4 import BeautifulSoup
 import contractions
+import re
 
 class BilingualDataset(Dataset):
 
@@ -38,9 +39,13 @@ def clean_data(text, lang):
     text = (text.lower()).replace(" '", "'")
     if lang == "en":
         text = contractions.fix(text)
-    if lang == "vi":
-        text = text.replace("you", "u")
-    return text
+    return handle_special_char(text)
+
+def handle_special_char(sent):
+    sent = re.sub(r'([.,!?;(){}\[\]])', r' \1 ', sent)
+    sent = re.sub(r'\s{2,}', ' ', sent)
+    sent = sent.strip()
+    return sent
 
 def handle_lang_vi(sent, lang, config):
     if lang == "vi" and config["underthesea"]:
